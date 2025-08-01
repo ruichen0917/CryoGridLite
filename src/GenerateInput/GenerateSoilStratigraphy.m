93f8a0194df32dd944f9a9288099dec0a2c1bcb2
Rui Chen <57906096+ruichen0917@users.noreply.github.com>
2025-08-01 10:56:47 +0200

    Delete src directory

--- src/GenerateInput/GenerateSoilStratigraphy.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = GenerateSoilStratigraphy(inputArg1,inputArg2)
-%GENERATESOILSTRATIGRAPHY Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
